a699bc226bdd8e092d9490a1c50cc6a25c1f7551
MaikolRuiz04 <contact@maikolruiz.com>
2025-09-24 21:00:54 -0400

    Port

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,34 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
 
-#ifndef LED_BUILTIN
-#define LED_BUILTIN 2   // Most ESP32 boards use GPIO2 for onboard LED
-#endif
+#define TFT_CS   5
+#define TFT_DC   21
+#define TFT_RST  22
+#define TFT_SCK  18
+#define TFT_MISO 19
+#define TFT_MOSI 23
+
+Adafruit_ILI9341 tft(TFT_CS, TFT_DC, TFT_RST);
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
-  Serial.println("ESP32 Blink Test: setup done");
+  SPI.begin(TFT_SCK, TFT_MISO, TFT_MOSI);
+  // If it still stays black, try lowering speed:
+  // SPI.setFrequency(16000000);
+
+  tft.begin();
+  tft.setRotation(1);
+
+  tft.fillScreen(ILI9341_RED);   delay(300);
+  tft.fillScreen(ILI9341_GREEN); delay(300);
+  tft.fillScreen(ILI9341_BLUE);  delay(300);
+  tft.fillScreen(ILI9341_BLACK);
+  tft.setTextColor(ILI9341_WHITE);
+  tft.setTextSize(2);
+  tft.setCursor(16,16);
+  tft.println("Hello ILI9341!");
 }
 
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  Serial.println("LED ON");
-  delay(500);
-
-  digitalWrite(LED_BUILTIN, LOW);
-  Serial.println("LED OFF");
-  delay(500);
-}
+void loop() {}
